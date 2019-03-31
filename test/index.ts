7c26c38e5d81700356389f5c0ca70acbe655a005
Danny Fritz <dannyfritz@gmail.com>
2019-03-31 08:36:44 -0600

    🚨 change to ES2016 for Node v6

--- test/index.ts
+++ test/index.ts
@@ -9,17 +9,24 @@ import EbirdClient, { Detail } from "../src";
 let ebirdClient: EbirdClient;
 test.beforeEach((_t) => {
   ebirdClient = new EbirdClient(process.env.API_KEY!);
-})
+});
 
-const denver = {
+const DENVER = {
   latitude: 39.742043,
   longitude: -104.991531,
 };
 
+test("adjacentRegions", async (t) => {
+  const data = await ebirdClient.adjacentRegions({
+    regionCode: "US-CO",
+  });
+  t.assert(_.isArray(data));
+});
+
 test("recentNearbyNotableObservations", async (t) => {
   const data = await ebirdClient.recentNearbyNotableObservations({
-    lat: denver.latitude,
-    lng: denver.longitude,
+    lat: DENVER.latitude,
+    lng: DENVER.longitude,
     maxResults: 2,
   });
   t.assert(_.isArray(data));
