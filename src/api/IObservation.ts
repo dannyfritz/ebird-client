a6c5a251ead2fbdca4ac7ef937dcefe09e3148c2
Danny Fritz <dannyfritz@gmail.com>
2020-01-24 08:20:42 -0700

    include some stronger typings for IObservation

--- src/api/IObservation.ts
+++ src/api/IObservation.ts
@@ -1,4 +1,6 @@
-export interface IObservation {
+export type IObservation = IObservationSimple | IObservationFull;
+
+export interface IObservationSimple {
   speciesCode: string;
   comName: string;
   sciName: string;
@@ -11,4 +13,23 @@ export interface IObservation {
   obsValid: boolean;
   obsReviewed: boolean;
   locationPrivate: boolean;
+}
+
+export type IObservationFull = IObservationSimple & {
+  subnational2Code: string,
+  subnational2Name: string,
+  subnational1Code: string,
+  subnational1Name: string,
+  countryCode: string,
+  countryName: string,
+  userDisplayName: string,
+  subId: string,
+  obsId: string,
+  checklistId: string,
+  presenceNoted: boolean,
+  hasComments: boolean,
+  firstName: string,
+  lastName: string,
+  hasRichMedia: boolean,
+  locID: string,
 }
